7534dae1eab04822bee18ab455f7f1d44c34c063
theprojectsomething <som@theprojectsomething.com>
2020-09-16 23:02:14 +1000

    test with latest fauxton build

--- prepublish.sh
+++ prepublish.sh
@@ -8,7 +8,7 @@ set -e
 # We alter it, rebuild it, and then move the built assets into /fauxton
 #
 
-GIT_HASH=b940979
+GIT_HASH=ce76aac
 
 FAUXTON_SRC=$(pwd)/fauxton-src
 REPLACE=$(pwd)/node_modules/.bin/replace
